6b24ff4710c046ffb0edf77dbfd76c4668fdb433
LUIS1508-EC <151102084+LUIS1508-EC@users.noreply.github.com>
2024-06-17 22:50:31 -0400

    .....

--- pokemones/src/App.jsx
+++ pokemones/src/App.jsx
@@ -4,8 +4,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Pokemones from './assets/pages/Pokemones'
 import Detalle from './assets/pages/Detalle'
-<<<<<<< HEAD
-import Home from './assets/pages/Home1'
+<<<<<<< HEADimport Home from './assets/pages/Home1'
 =======
 import Home from './Home1'
 >>>>>>> 0bf054e67603d233d3cb901dda6fbb29f157f342
--- pokemones/src/assets/pages/Detalle.jsx
+++ pokemones/src/assets/pages/Detalle.jsx
@@ -28,12 +28,11 @@ const Detalle = () => {
     }, [name])
 
     return (
-<<<<<<< HEAD
         <div>
             {pokemon ? (
                 <>
                     <div className='card'>
-                        <h1 className='text-transform-capitalize d-flex justify-content-center card-title'>{pokemon.name}</h1>
+                        <h1 className='font3 d-flex justify-content-center card-title'>{pokemon.name}</h1>
                         <div className='d-flex justify-content-center'>
                             <h3 className="text-align-center text-xl mt-2 uppercase">
                                 {""}
@@ -77,59 +76,6 @@ const Detalle = () => {
             ) : null}
 
         </div>
-=======
-
-        <div className='p-4 d-flex justify-content-center'>
-            {pokemon ? (
-                <>
-                    <card className="card mb3 card2">
-                        <div className='row'>
-                            <div className='col-md-4 image'>
-                                {pokemon.sprites ? (
-                                    <img src={pokemon.sprites?.front_default} alt="Imagen Pokemon" width={300} />
-                                ) : null}
-                            </div>
-                            <div className="col-md-8">
-                                <div className="card-body">
-                                    <h1 className='card-title'>{pokemon.name}</h1>
-                                    <div>
-                                        <h5>
-                                            Tipo : {pokemon.types ? (
-                                            pokemon.types.map((type, index) => (
-                                                <p
-                                                    key={index}
-                                                >
-                                                    {index > 0 ? " , " : ""}
-                                                    {type.type.name}
-                                                </p>
-                                            ))
-                                        ) : (
-                                            <h3>Desconocido</h3>
-                                        )}
-                                        </h5>
-
-                                        
-                                    </div>
-                                    <div className='p-5 m-4'>
-                                        {pokemon.stats && pokemon.stats.map((stat) => (
-                                            <div className='d-flex' key={stat.stat.name}>
-                                                <label className='font-bold'>'{stat.stat.name} : </label>
-                                                <p>{stat.base_stat}</p>
-                                            </div>
-                                        ))}
-                                    </div>
-                                                                      
-
-                                </div>
-                            </div >
-                        </div>
-                    </card>
-                </>
-            ) : null}
-
-        </div>
-
->>>>>>> 0bf054e67603d233d3cb901dda6fbb29f157f342
     )
 
 }
